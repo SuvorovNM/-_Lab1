f791b93b7b97c31a3fc9772ccf8f0cfcb3cd6901
SuvorovNM <35630837+SuvorovNM@users.noreply.github.com>
2020-01-18 21:58:49 +0500

    Created v1 of Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,10 +6,119 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
+    abstract class Kuzov
+    {
+        public abstract void Show();
+    }
+    class Universal : Kuzov
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Универсальный кузов");
+        }
+    }
+    class Sedan : Kuzov
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Седан-кузов");
+        }
+    }
+    class Kupe : Kuzov
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Купе-кузов");
+        }
+    }
+    class Engine
+    {
+        int EnginePower;
+        double FuelConsumption;
+        public Engine(int power, double consumption)
+        {
+            EnginePower = power;
+            FuelConsumption = consumption;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Мощность двигателя "+ EnginePower + " л.с.");
+            Console.WriteLine("Расход топлива "+ FuelConsumption + "л. на 100км");
+        }
+    }
+    abstract class AutoFactory
+    {
+        public abstract Kuzov CreateCuzov();
+        public abstract Engine CreateEngine(int power, double consumption);
+    }
+    class LadaFactory : AutoFactory
+    {
+        public override Kuzov CreateCuzov()
+        {
+            return new Sedan();
+        }
+
+        public override Engine CreateEngine(int power, double consumption)
+        {
+            return new Engine(power, consumption);
+        }
+    }
+    class AudiFactory : AutoFactory
+    {
+        public override Kuzov CreateCuzov()
+        {
+            return new Universal();
+        }
+
+        public override Engine CreateEngine(int power, double consumption)
+        {
+            return new Engine(power, consumption);
+        }
+    }
+    class BMWFactory : AutoFactory
+    {
+        public override Kuzov CreateCuzov()
+        {
+            return new Kupe();
+        }
+
+        public override Engine CreateEngine(int power, double consumption)
+        {
+            return new Engine(power, consumption);
+        }
+    }
+    //Client - автомобиль
+    class Automobile
+    {
+        private Engine engine;
+        private Kuzov kuzov;
+        //private string factory;
+        public Automobile(AutoFactory factory, int p, double c)
+        {
+            engine = factory.CreateEngine(p, c);
+            kuzov = factory.CreateCuzov();
+        }
+        public void ShowInfo()
+        {
+            engine.Show();
+            kuzov.Show();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Создаем Ладу Приору: ");
+            Automobile Lada_Priora = new Automobile(new LadaFactory(), 86, 5.6);
+            Lada_Priora.ShowInfo();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Создаем БМВ Х6: ");
+            Automobile BMW_X6 = new Automobile(new BMWFactory(), 400, 12.5);
+            BMW_X6.ShowInfo();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Singleton/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public enum Kuzov { Sedan, Universal, Kupe}
+    class Engine
+    {
+        int EnginePower;
+        double FuelConsumption;
+        public Engine(int power, double consumption)
+        {
+            EnginePower = power;
+            FuelConsumption = consumption;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Мощность двигателя " + EnginePower + " л.с.");
+            Console.WriteLine("Расход топлива " + FuelConsumption + "л. на 100км");
+        }
+    }
+    class Automobile
+    {
+        private static Automobile instance;
+
+        public Engine Engine { get; private set; }
+        public Kuzov Kuzov { get; private set; }
+        public string Factory { get; private set; }
+
+        protected Automobile(Engine eng, Kuzov kuz, string fac)
+        {
+            this.Engine = eng;
+            this.Kuzov = kuz;
+            this.Factory = fac;
+        }
+        public static Automobile getInstance(Engine eng, Kuzov kuz, string fac)
+        {
+            if (instance == null)
+            {
+                instance = new Automobile(eng, kuz, fac);                
+            }
+            return instance;
+        }
+    }
+
+    class CreatingAuto
+    {
+        public Automobile Automobile { get; set; }
+
+        public void CreateAutomobile(Engine eng, Kuzov kuz, string fac)
+        {
+            Automobile = Automobile.getInstance(eng, kuz, fac);
+            Automobile.Engine.Show();
+            Console.WriteLine(Automobile.Kuzov.ToString(), Automobile.Factory);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Создаем Ладу Приору: ");
+
+            CreatingAuto LadaPriora = new CreatingAuto();
+            LadaPriora.CreateAutomobile(new Engine(86, 5.6), Kuzov.Sedan, "АЦ Лада");
+
+            Console.WriteLine();
+            Console.WriteLine("Пытаемся заменить Приору на Беху: ");
+            LadaPriora.CreateAutomobile(new Engine(400, 12.5), Kuzov.Kupe, "АЦ BMW");
+
+            Console.ReadLine();
+        }
+    }
+}
